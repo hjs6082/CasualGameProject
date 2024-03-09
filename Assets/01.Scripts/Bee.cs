4f89ab6eea904d74da6dd5daa012f9cfae4c1aa0
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2024-03-09 23:33:17 +0900

    feat: 벌에 힘 적용, TODO : UI 제작 및 스테이지 패널 제작 및 메인화면 제작

--- Assets/01.Scripts/Bee.cs
+++ Assets/01.Scripts/Bee.cs
@@ -6,15 +6,18 @@ public class Bee : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform player;
+    [SerializeField] private float pushForce = 500f; // 벌이 밀려나는 힘
+    [SerializeField] private float obstaclePushForce = 200f; // 장애물을 미는 힘
 
     private Rigidbody2D rb;
     private Vector2 movement;
-    
+
     private void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         player = GameManager.Instance.Player.transform;
     }
+
     private void Update()
     {
         Vector3 direction = player.position - transform.position;
@@ -23,12 +26,37 @@ public class Bee : MonoBehaviour
         direction.Normalize();
         movement = direction;
     }
+
     private void FixedUpdate()
     {
         MoveCharacter(movement);
     }
+
     private void MoveCharacter(Vector2 direction)
     {
         rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            // 장애물을 밀어낼 때 사용할 방향 (벌에서 장애물로의 방향)
+            Vector2 pushDirection = collision.transform.position - transform.position;
+            pushDirection.Normalize();
+
+            // 장애물에 힘을 가함
+            collision.rigidbody.AddForce(pushDirection * obstaclePushForce);
+
+            // 벌 자신이 반대 방향으로 밀려남
+            rb.AddForce(-pushDirection * pushForce);
+        }
+        else if(collision.gameObject.CompareTag("Player"))
+        {
+            Vector2 pushDirection = collision.transform.position - transform.position;
+
+            rb.AddForce(-pushDirection * pushForce);
+        }
+
     }
 }
