6089177b56ddb21683bff95986af47d710ad7d38
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2024-04-14 22:58:39 +0900

    feat: 구글 연동

---
+++ Assets/01.Scripts/Google/GPGSManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class GPGSManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI logText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+
+    public void Login()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        //유저가 변경 가능한 닉네임
+        string displayName = PlayGamesPlatform.Instance.GetUserDisplayName();
+        //변경 불가한 고유 ID
+        string userID = PlayGamesPlatform.Instance.GetUserId();
+
+        if (status == SignInStatus.Success)
+        {
+            Debug.Log("Login Success : " + displayName + " / " + userID);
+            logText.text = "Login Success : " + displayName + " / " + userID;
+        }
+        else
+        {
+            Debug.Log("Login Fail");
+            logText.text = "Login Fail";
+        }
+    }
+
+}
